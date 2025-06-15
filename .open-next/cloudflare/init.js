265120c08cbfee73c5a5b55082e6e4304328e907
chandrabs25 <22f3002048@ds.study.iitm.ac.in>
2025-06-15 21:44:33 +0530

    commit

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -48,7 +48,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1749919420064,
+    __BUILD_TIMESTAMP_MS__: 1749994410708,
     __NEXT_BASE_PATH__: ""
   });
 }
